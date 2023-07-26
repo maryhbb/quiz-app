4d4efd8af1c693522c4b33fbf7b8a2a41c30cc8c
Mary <76174247+maryhbb@users.noreply.github.com>
2023-07-26 13:14:59 +0200

    Add questions and answers as data in cards array

--- js/card/data.js
+++ js/card/data.js
@@ -1,29 +1,38 @@
 export const cards = [
   {
     id: 1,
-    question: "What is Js?",
-    answer: "is bla bla",
+    question: "What is HTML?",
+    answer: "HyperText Markup Language for web page structure.",
     tags: ["css", "html", "js"],
     bookmarked: false,
   },
   {
     id: 2,
-    question: "What is scss?",
-    answer: "is blub blub",
+    question: "What is CSS?",
+    answer: "Cascading Style Sheets for web page design.",
     tags: ["sql", "grid", "flexbox"],
     bookmarked: true,
   },
   {
     id: 3,
-    question: "What is React?",
-    answer: "is bla bla",
+    question:
+      "What is the difference between frontend and backend development?",
+    answer:
+      "Frontend - User interface development. Backend - Server-side development.",
     tags: ["css", "html", "js"],
     bookmarked: false,
   },
   {
     id: 4,
-    question: "What is flexbox?",
-    answer: "is blub blub",
+    question: "What is responsive web design?",
+    answer: "Design adapting to different devices and screen sizes.",
+    tags: ["sql", "grid", "flexbox"],
+    bookmarked: true,
+  },
+  {
+    id: 4,
+    question: "What are JavaScript frameworks?",
+    answer: "Pre-written libraries for easier web app development.",
     tags: ["sql", "grid", "flexbox"],
     bookmarked: true,
   },
