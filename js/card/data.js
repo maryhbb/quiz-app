7c71d2a5c701a9008d6f8dafce785e4c2c278224
Mary <76174247+maryhbb@users.noreply.github.com>
2023-07-29 13:16:04 +0200

    Add uuid for cards

--- js/card/data.js
+++ js/card/data.js
@@ -1,20 +1,19 @@
+import { v4 as uuidv4 } from "https://jspm.dev/uuid";
+
 export const cards = [
   {
-    id: 1,
     question: "What is HTML?",
     answer: "HyperText Markup Language for web page structure.",
     tags: ["css", "html", "js"],
     bookmarked: false,
   },
   {
-    id: 2,
     question: "What is CSS?",
     answer: "Cascading Style Sheets for web page design.",
     tags: ["sql", "grid", "flexbox"],
     bookmarked: true,
   },
   {
-    id: 3,
     question:
       "What is the difference between frontend and backend development?",
     answer:
@@ -23,17 +22,19 @@ export const cards = [
     bookmarked: false,
   },
   {
-    id: 4,
     question: "What is responsive web design?",
     answer: "Design adapting to different devices and screen sizes.",
     tags: ["sql", "grid", "flexbox"],
     bookmarked: true,
   },
   {
-    id: 4,
     question: "What are JavaScript frameworks?",
     answer: "Pre-written libraries for easier web app development.",
     tags: ["sql", "grid", "flexbox"],
     bookmarked: true,
   },
 ];
+
+cards.forEach((card) => {
+  card.id = uuidv4();
+});
