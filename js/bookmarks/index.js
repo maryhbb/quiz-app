d0e5d156375bbe7c143094defb534ec94c45e9f8
Mary <76174247+maryhbb@users.noreply.github.com>
2023-08-01 00:25:21 +0200

    Remove unbookmarked card from bookmark page

--- js/bookmarks/index.js
+++ js/bookmarks/index.js
@@ -1,4 +1,4 @@
-import { appendCard, getAllCards } from "../card/index.js";
+import { appendCard, getAllCards, setAllCards } from "../card/index.js";
 import { flipCard } from "../flipCard.js";
 
 const container = document.querySelector("main");
@@ -7,3 +7,31 @@ getAllCards()
   .filter((card) => card.bookmarked)
   .forEach((card) => appendCard(card, container));
 flipCard();
+
+function removeBookmark(event) {
+  // find button element
+  const toggleBtn = event.currentTarget;
+  console.log(toggleBtn);
+
+  //find card ID
+  const cardContainerElement = toggleBtn.closest("[data-card-id]");
+  console.log(cardContainerElement);
+
+  const cardId = cardContainerElement.getAttribute("data-card-id");
+  console.log(cardId);
+
+  //find corresponding card from localStorage
+  const updatedCards = getAllCards().map((card) => {
+    if (card.id === cardId) {
+      card.bookmarked = !card.bookmarked;
+    }
+    return card;
+  });
+
+  setAllCards(updatedCards);
+  cardContainerElement.remove();
+}
+const cardBookmarks = document.querySelectorAll(".card__bookmark");
+for (const cardBookmark of cardBookmarks) {
+  cardBookmark.onclick = removeBookmark;
+}
