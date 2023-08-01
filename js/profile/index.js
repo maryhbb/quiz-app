f8c8c90a254d9f6d42c46e0f0b0ae8c31f2300f5
Mary <76174247+maryhbb@users.noreply.github.com>
2023-08-01 14:50:02 +0200

    feat: Implement question and bookmark counts on profile page

--- js/profile/index.js
+++ js/profile/index.js
@@ -1,2 +1,12 @@
 import { registerThemeToggle } from "../darkMode.js";
+import { getAllCards } from "../card/index.js";
 registerThemeToggle();
+
+/**
+ * Updates the question and bookmarked count functionality on the profile page.
+ */
+const cards = getAllCards();
+
+document.querySelector('[data-js="quation-count"]').textContent = cards.length;
+document.querySelector('[data-js="bookmarks-count"]').textContent =
+  cards.filter((card) => card.bookmarked).length;
